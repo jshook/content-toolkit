d62100d42f50f8504b9788e5efd6790d2aeb41c7
Jonathan Shook <jshook@gmail.com>
2017-04-18 13:35:40 -0500

    updated for --network=host

--- sourceme.bash
+++ sourceme.bash
@@ -1,1 +1,1 @@
-alias hugo="sudo docker run --rm -ti -v $(pwd):$(pwd) -w $(pwd) -p 1313:1313 jshook/content-toolkit"
+alias hugo="sudo docker run --rm -ti -v $(pwd):$(pwd) -w $(pwd) --network="host" -p 1313:1313 jshook/content-toolkit"
